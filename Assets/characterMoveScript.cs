2de6e05da24aafe4d75a38cebebca5f326c89859
wormmeatapple <127395841+wormmeatapple@users.noreply.github.com>
2024-08-18 12:27:58 +0800

    Add files via upload

---
+++ Assets/characterMoveScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+public class characterMoveScript : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    public InputAction playerControls;
+    public Rigidbody2D rb;
+
+    Vector2 moveDirection = Vector2.zero;
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = playerControls.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
